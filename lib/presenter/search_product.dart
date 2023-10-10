9d546c54b51d8b4bd7bd6c4f15fec8648e5882ec
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2023-10-10 07:51:52 +0300

    feat: new widget SearchWidget

--- lib/presenter/search_product.dart
+++ lib/presenter/search_product.dart
@@ -5,11 +5,55 @@ class SearchProductWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final width = MediaQuery.sizeOf(context).width;
+
+    return Scaffold(
       body: SafeArea(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 0),
           child: Column(
-        children: [],
-      )),
+            children: [
+              ImageProductWidget(size: width),
+              const SearchWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageProductWidget extends StatelessWidget {
+  final double size;
+
+  const ImageProductWidget({
+    super.key,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: const Image(
+        image: AssetImage("assetName"),
+      ),
+    );
+  }
+}
+
+class SearchWidget extends StatelessWidget {
+  const SearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: const TextField()),
+        ElevatedButton(onPressed: () {}, child: const Icon(Icons.search))
+      ],
     );
   }
 }
