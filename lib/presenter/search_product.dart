0ea138c8c20dfb8570b301a5cd03f10b07209703
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2023-10-11 13:41:28 +0300

    feat: new TextField

--- lib/presenter/search_product.dart
+++ lib/presenter/search_product.dart
@@ -13,9 +13,13 @@ class SearchProductWidget extends StatelessWidget {
           padding:
               const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ImageProductWidget(size: width),
+              const SizedBox(height: 20),
               const SearchWidget(),
+              const SizedBox(height: 20),
+              const TextInfoProduct()
             ],
           ),
         ),
@@ -51,9 +55,47 @@ class SearchWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Expanded(child: const TextField()),
-        ElevatedButton(onPressed: () {}, child: const Icon(Icons.search))
+        const Expanded(
+          child: SizedBox(
+            height: 60,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                isCollapsed: true,
+                contentPadding: EdgeInsets.all(20),
+                constraints:
+                    BoxConstraints(maxHeight: 10, minHeight: 10, maxWidth: 10),
+              ),
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size(60, 60),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: const Icon(Icons.search),
+        )
       ],
     );
+  }
+}
+
+class TextInfoProduct extends StatelessWidget {
+  const TextInfoProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("data");
   }
 }
