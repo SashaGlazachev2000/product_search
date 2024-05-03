c1134d0a0fbb29f6abf6e346b7afd0540cc87887
SashaGlazachev2000 <glazachev.sasha2000@gmail.com>
2024-05-03 15:41:44 +0300

    added barcode camera

--- lib/view/search_product/search_widget.dart
+++ lib/view/search_product/search_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:product_search/assets/app_colors.dart';
 import 'package:product_search/bloc/product_bloc/product_bloc.dart';
@@ -61,6 +63,8 @@ class SearchWidget extends StatelessWidget {
               ),
             ),
             const SizedBox(width: 10),
+            const _ButtonBacrode(),
+            const SizedBox(width: 10),
             ElevatedButton(
               onPressed: () {
                 context.read<ProductBloc>().add(
@@ -81,6 +85,54 @@ class SearchWidget extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class _ButtonBacrode extends StatefulWidget {
+  const _ButtonBacrode({
+    super.key,
+  });
+
+  @override
+  State<_ButtonBacrode> createState() => _ButtonBacrodeState();
+}
+
+class _ButtonBacrodeState extends State<_ButtonBacrode> {
+  Future<void> _scanBarcode() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.DEFAULT,
+      );
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    context.read<ProductBloc>().add(
+          ProductGetProductEvent(
+            code: int.tryParse(barcodeScanRes),
+          ),
+        );
+    FocusManager.instance.primaryFocus?.unfocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _scanBarcode,
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(60, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: const Icon(Icons.qr_code_scanner_sharp),
     );
   }
 }
